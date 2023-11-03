fd2879b75033e843820660928f5454170f290943
zhangtienyong <tienhanprab@gmail.com>
2023-11-03 19:41:11 +0800

    update logger and controllers services

--- server.ts
+++ server.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import path from "path";
 import { apiRoutes } from "./routers/routes";
+import logger from "./utils/logger";
+
 
 const app = express();
 
 app.use("/", apiRoutes);
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = 8080;
 app.listen(PORT, () => {
-  console.log(`listening at http://localhost:${PORT}`);
+  logger.info(`Server is listening at http://localhost:${PORT}`);
 });
