ff245602afe011831f8c2b975874880cd607f6ca
Joe <kusiuwa@gmail.com>
2023-11-14 14:11:14 +0800

    Revert "Merge branch 'main' of https://github.com/zhangtienyong/bad-project-ai-powered-predict-code-pay"
    This reverts commit b3fb9b00c52a211402f53c8163149a0e07e37fc0, reversing
    changes made to 3c89afd6afd4b1813a7bf973ebc5d1f221137811.

--- web-application/controllers/jobposting.controller.ts
+++ web-application/controllers/jobposting.controller.ts
@@ -11,13 +11,13 @@ export default class JobPostingController {
 
         try {
             const skills = await this.jobPostingService.getSkills();
+            
+            const programming_language_skills = skills.programming_language_skills.map(sk =>sk.name)
+            const database_skills = skills.database_skills.map(sk =>sk.name)
+            const web_framework_skills = skills.web_framework_skills.map(sk =>sk.name)
+            const cloud_platform_skills = skills.cloud_platform_skills.map(sk =>sk.name)
 
-            const programming_language_skills = skills.programming_language_skills.map(sk => sk.name)
-            const database_skills = skills.database_skills.map(sk => sk.name)
-            const web_framework_skills = skills.web_framework_skills.map(sk => sk.name)
-            const cloud_platform_skills = skills.cloud_platform_skills.map(sk => sk.name)
-
-            res.status(200).json({ result: { programming_language_skills, database_skills, web_framework_skills, cloud_platform_skills } })
+            res.status(200).json({result: {programming_language_skills, database_skills, web_framework_skills, cloud_platform_skills}})
         } catch (err) {
             console.error(err.message);
             res.status(500).json({ message: "internal server error" });
@@ -30,25 +30,14 @@ export default class JobPostingController {
 
         try {
 
-            const {
-                job_title,
-                experience_level,
-                work_place,
-                employment_type,
-                job_description,
-                responsibilities,
-                qualifications,
-                programming_language_skills,
-                database_skills,
-                cloud_platform_skills,
-                web_framework_skills,
-            } = req.body;
-
+            const { job_title, work_place, employment_type, job_description, experience_level, responsibilities, qualifications, skills } = req.body;
+    
             const loginUser = req.session.user;
             const loginUserId = loginUser?.user_id!;
+           
 
-            await this.jobPostingService.jobPosting(loginUserId, job_title, work_place, employment_type, job_description, experience_level, responsibilities, qualifications, programming_language_skills, database_skills, cloud_platform_skills, web_framework_skills);
-            res.json({ success: true, message: "Job posting successful" });
+            await this.jobPostingService.jobPosting(loginUserId, job_title, work_place, employment_type, job_description, experience_level, responsibilities, qualifications, skills);
+
         } catch (err) {
             console.error(err.message);
             res.status(500).json({ message: "internal server error" });
