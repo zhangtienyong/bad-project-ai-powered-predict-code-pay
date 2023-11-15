4d1fc8fc3f88ba869f2fe81b47eda9ed6891e370
Joe <kusiuwa@gmail.com>
2023-11-15 11:03:45 +0800

    fix lost code

--- web-application/public/javascript/jobboards.js
+++ web-application/public/javascript/jobboards.js
@@ -11,7 +11,6 @@ async function initJobBoard(page) {
 
   const res = await fetch(`/jobboards/initJobBoard?page=${page}`);
   const JobBoards = await res.json();
-  console.log(JobBoards)
 
   const jobBoardContainerEle = document.querySelector(".job-container");
   const templateEle = document.querySelector("#JOB-template");
@@ -19,7 +18,9 @@ async function initJobBoard(page) {
 
   for (const jobBoard of JobBoards) {
     const jobBoardClone = templateEle.content.cloneNode(true);
-    jobBoardClone.querySelector(".company").textContent = jobBoard.id
+    jobBoardClone.querySelector(".image").src = jobBoard.logo
+    jobBoardClone.querySelector(".id").textContent = jobBoard.id
+    jobBoardClone.querySelector(".company").textContent = jobBoard.company_name
     jobBoardClone.querySelector(".title").textContent = `Job Position: ${jobBoard.job_title}`
     jobBoardClone.querySelector(".type").textContent = `Type: ${jobBoard.employment_type}`
     jobBoardClone.querySelector(".place").textContent = `Work place: ${jobBoard.work_place}`
@@ -27,16 +28,39 @@ async function initJobBoard(page) {
   }
 }
 
+async function jobDetails(button) {
+  const listItem = button.closest('.custom-card');
+  const IDElement = listItem.querySelector('.id');
+  const ID = parseFloat(IDElement.textContent.trim());
+  console.log(ID)
+
+// try {
+//   const response = await fetch("/dashboard/employer/accepted_job", {
+//     method: "POST",
+//     body: JSON.stringify({ ID }), 
+//     headers: {
+//       "Content-type": "application/json",
+//     },
+//   });
+//       if (response.ok) {
+//         await response.json();
+//       } 
+//       else {
+//       console.error('Error:', res.status);
+//       }
+//     } catch (error) {
+//       console.error("Error:", error);
+//     }  
+}
+
 function loadNextPage() {
   currentPage++;
-  console.log(currentPage)
   initJobBoard(currentPage);
 }
 
 function loadPreviousPage() {
   if (currentPage > 1) {
     currentPage--;
-    console.log(currentPage)
     initJobBoard(currentPage);
   }
 }
