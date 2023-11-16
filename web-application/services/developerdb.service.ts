ab4036fb27e91809096217287f6635cbb6098dd8
Joe <kusiuwa@gmail.com>
2023-11-16 12:39:21 +0800

    fix developer service

--- web-application/services/developerdb.service.ts
+++ web-application/services/developerdb.service.ts
@@ -8,39 +8,20 @@ export default class DeveloperDbService {
 
   async application(id: number, app: number, pageSize: number = 5) {
     try {
-      const company = await this.knex("company")
-        .select("id", "user_id")
-        .where("user_id", id)
-        .first();
-
-      if (company) {
-        const offset = (app - 1) * pageSize;
-
-        const jobs = await this.knex("jobs")
+      const offset = (app - 1) * pageSize;
+        const applications = await this.knex("job_applications")
           .select("*")
-          .where("company_id", company.id)
-
-        if (jobs && jobs.length > 0) {
-          const jobIds = jobs.map((job) => job.id);
-          const applications = await this.knex("job_applications")
-            .select("*")
-            .whereIn("job_id", jobIds)
-            .where("status", "Pending")
-            .limit(pageSize)
-            .offset(offset);
-
-          return applications;
-        }
-
-        return
-
-      } else {
-        throw new Error("Company not found for the given user");
+          .where("user_id", id)
+          .limit(pageSize)
+          .offset(offset);
+  
+        return applications;
       }
-    } catch (error) {
+     catch (error) {
       throw error;
     }
   }
+  
 
 
 
