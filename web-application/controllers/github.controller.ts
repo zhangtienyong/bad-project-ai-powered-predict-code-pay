eeb1e31bcb17499e458949830328fa07b63d6054
Joe <kusiuwa@gmail.com>
2023-11-17 03:22:17 +0800

    Update github.controller.ts

--- web-application/controllers/github.controller.ts
+++ web-application/controllers/github.controller.ts
@@ -19,7 +19,7 @@ employee_callback = async (req: Request, res: Response) => {
         client_id: process.env.EMPLOYEE_CLIENT_ID,
         client_secret: process.env.EMPLOYEE_CLIENT_SECRET,
         code,
-        redirect_uri: `https://predictcodepay.me:${process.env.PORT}/signin/employee_callback`
+        redirect_uri: `http://predictcodepay.me:${process.env.PORT}/signin/employee_callback`
     };
 
     const opts = {
